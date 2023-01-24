cea76f6700c51852c4b561b83eaefb9d654bcb46
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-24 21:15:48 +0900

    main.tsx Home pages로 router 셋팅

--- main.tsx
+++ main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./src/view/pages/Home";
 import "./index.css";
 
 const App = () => {
-  return <div>App 컴포넌트</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
