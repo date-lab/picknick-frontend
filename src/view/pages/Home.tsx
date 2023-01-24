2df38ef0b2d37910c01411b0fcc056de01c176b7
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-24 21:53:41 +0900

    스플래시 기능 적용

--- src/view/pages/Home.tsx
+++ src/view/pages/Home.tsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
-import Splash from "view/components/Splash";
+import Splash from "../components/Splash";
 
 const Home = () => {
+  const [splash, setSplash] = useState<boolean>(true);
+
+  setTimeout(() => {
+    setSplash(false);
+  }, 1000);
+  if (splash) return <Splash />;
   return <div>홈</div>;
 };
 
