69995e4c167341483dc374706631a6e818ec7a7b
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-02-05 01:04:48 +0900

    MapLocation 컴포넌트 css 임시 적용

--- src/view/components/MapLocationCSS.tsx
+++ src/view/components/MapLocationCSS.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export const MapWrap = styled.div`
+  width: 100%;
+  height: 100vh;
   font-size: 22px;
   font-weigth: bold;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
